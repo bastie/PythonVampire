7ae90103b565aeaa23a9cd9ac4c6511a5c653cca
Sͬeͥbͭaͭsͤtͬian <sebastian.ritter@example.com>
2020-09-05 16:41:54 +0200

    added information for development

--- src/build.py
+++ src/build.py
@@ -5,12 +5,27 @@
 @author: Sͬeͥbͭaͭsͤtͬian
 '''
 
+
+# pip3 install -U --requirement requirements.txt 
+# pip3 install -U --requirement requirements-devel.txt 
+
+
 # if we are in src directory
-# 
-# $ rm -Rf VampireAPI.egg-info dist build 
-# python setup.py sdist bdist_wheel    
+#
+# clear 
+# $ rm -Rf VampireAPI.egg-info dist build ../docs
+#
+# build module
+# python3 setup.py sdist bdist_wheel    
 # python3 -m twine check dist/*
-# python3 -m twine upload dist/*    
-# git add .
+# python3 -m twine upload dist/*
+#
+# build docs
+# python3 -m pdoc --html --output-dir ../docs/_build/html java
+#
+# add all
+#    
+# git add ../.
 # git commit
 # git push 
+#
